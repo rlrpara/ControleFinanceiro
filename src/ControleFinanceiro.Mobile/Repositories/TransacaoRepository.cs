a680480691cd16eb7414a1475ad3f941a2f49b01
Rodrigo L Ribeiro <rlr.para@gmail.com>
2024-10-25 22:48:11 -0300

    adicionado crud repository transaction

--- src/ControleFinanceiro.Mobile/Repositories/TransacaoRepository.cs
+++ src/ControleFinanceiro.Mobile/Repositories/TransacaoRepository.cs
@@ -1,28 +1,24 @@
 ﻿using ControleFinanceiro.Mobile.Interfaces;
 using ControleFinanceiro.Mobile.Models;
+using LiteDB;
 
 namespace ControleFinanceiro.Mobile.Repositories;
 
 public class TransacaoRepository : ITransacaoRepository
 {
-    public TransacaoRepository()
-    {
-        
-    }
-    public List<Transacao> ObterTodos()
-    {
+    #region [Private Properties]
+    private readonly LiteDatabase _database;
+    private readonly string CollectionName = "transactions";
+    #endregion
 
-    }
-    public void Adicionar(Transacao transacao)
-    {
+    #region [Constructor]
+    public TransacaoRepository() => _database = new LiteDatabase("Filename=");
+    #endregion
 
-    }
-    public void Atualizacao(Transacao transacao)
-    {
-
-    }
-    public void Deletar(int id)
-    {
-
-    }
+    #region [Public Methods]
+    public List<Transacao> ObterTodos() => _database.GetCollection<Transacao>(CollectionName).Query().OrderByDescending(x => x.Data).ToList();
+    public void Adicionar(Transacao transacao) => _database.GetCollection<Transacao>(CollectionName).Insert(transacao);
+    public void Atualizacao(Transacao transacao) => _database.GetCollection<Transacao>(CollectionName).Update(transacao);
+    public void Deletar(int id) => _database.GetCollection<Transacao>(CollectionName).Delete(id);
+    #endregion
 }
