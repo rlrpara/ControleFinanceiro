bd9d3559c9ee17e448784288563bc078467e57c6
Rodrigo L Ribeiro <rlr.para@gmail.com>
2024-10-25 23:10:31 -0300

    ajustes

--- src/ControleFinanceiro.Mobile/App.xaml.cs
+++ src/ControleFinanceiro.Mobile/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ControleFinanceiro.Mobile
+﻿using ControleFinanceiro.Mobile.Views;
+
+namespace ControleFinanceiro.Mobile
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new TransacaoLista());
         }
     }
 }
